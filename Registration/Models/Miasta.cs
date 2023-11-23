36feea43cca8c72e77e92217d13a981568fe6265
Mateusz <golonkamateusz99@gmail.com>
2023-11-23 19:38:06 +0100

    Dodaj pliki projektów.

---
+++ Registration/Models/Miasta.cs
@@ -0,0 +1,11 @@
+﻿namespace Registration.Models
+{
+    public enum Miasta
+    {
+        Krakow = 1,
+        Warszawa = 2,
+        Gdansk = 3,
+        Wroclaw = 4,
+        Rzeszow = 5
+    }
+}
